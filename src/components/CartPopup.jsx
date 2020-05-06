e881c7a210c550f5b878b8a5911caea922123630
Ivan <followhappyq@gmail.com>
2020-05-07 00:15:34 +0200

    cartpopup fixed

--- src/components/CartPopup.jsx
+++ src/components/CartPopup.jsx
@@ -3,22 +3,20 @@ import { List, Button, Image } from "semantic-ui-react"
 
 const CartPopup = ({ book, removeFromCart }) => {
   return (
-    <List selection verticalAlign="middle">
+    <List selection divided verticalAlign="middle">
       <List.Item>
-        <Image avatar src={book.image} />
-        <List.Content>
-          <List.Header>
-            {book.title}{" "}
-            <Button
-              onClick={() => {
-                removeFromCart(book.id)
-              }}
-              color="red"
-            >
-              X
-            </Button>
-          </List.Header>
+        <List.Content floated="right">
+          <Button
+            onClick={() => {
+              removeFromCart(book.id)
+            }}
+            color="red"
+          >
+            X
+          </Button>
         </List.Content>
+        <Image avatar src={book.image} />
+        <List.Content>{book.title}</List.Content>
       </List.Item>
     </List>
   )
